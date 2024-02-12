b1ac745b20f553372f8cfa0f2731028ba5251d39
allegria25 <allegriakinzoladev@gmail.com>
2024-02-12 08:00:53 +0100

    hah

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import postsReducer from '../features/posts/postSlice'
 import usersSlice from '../features/users/usersSlice'
+import notificationsclice from '../features/notifications/notificationsclice'
 
 export const store = configureStore({
   reducer : {
     posts : postsReducer,
-    users : usersSlice
+    users : usersSlice,
+    notifications : notificationsclice
   }
   
 })
