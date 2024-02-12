0f5063d63152030ce0fa66d899fe140898369841
allegria25 <allegriakinzoladev@gmail.com>
2024-02-12 08:03:43 +0100

    gfg

--- src/app/store.js
+++ src/app/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import postsReducer from '../features/posts/postSlice'
 import usersSlice from '../features/users/usersSlice'
 import notificationsclice from '../features/notifications/notificationsclice'
+import { apiSlice } from '../features/api/apiSlice'
 
 export const store = configureStore({
   reducer : {
     posts : postsReducer,
     users : usersSlice,
-    notifications : notificationsclice
+    notifications : notificationsclice,
+    [apiSlice.reducerPath]: apiSlice.reducer
   }
   
 })
