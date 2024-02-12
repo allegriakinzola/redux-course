015bc52b1cf8f18c4a69837e8bbd54a4a4906402
allegria25 <allegriakinzoladev@gmail.com>
2024-02-12 10:09:27 +0100

    change

--- src/features/posts/postslist.js
+++ src/features/posts/postslist.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useMemo } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostHautor } from './postauthor'
@@ -28,22 +28,31 @@ let PostExcerpt = ({ post }) => {
 
 export const PostsList = () => {
   const {
-    data: posts,
+    data: posts = [],
     isLoading,
     isSuccess,
     isError,
     error
   } = useGetPostsQuery()
 
+  const sortedPosts = useMemo(() => {
+    const sortedPosts = posts.slice()
+    // Sort posts in descending chronological order
+    sortedPosts.sort((a, b) => b.date.localeCompare(a.date))
+    return sortedPosts
+  }, [posts])
+
+
   let content
 
   if (isLoading) {
     content = <Spinner text="Loading..." />
   } else if (isSuccess) {
-    content = posts.map(post => <PostExcerpt key={post.id} post={post} />)
+    content = sortedPosts.map(post => <PostExcerpt key={post.id} post={post} />)
   } else if (isError) {
     content = <div>{error.toString()}</div>
   }
+
 
   return (
     <section className="posts-list">
