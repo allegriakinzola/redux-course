abd53c72dd6b91b88933c2a9f76f84d6a20bc5dd
allegria25 <allegriakinzoladev@gmail.com>
2024-02-12 13:17:20 +0100

    change

--- src/features/posts/singlepostpage.js
+++ src/features/posts/singlepostpage.js
@@ -2,28 +2,32 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom/cjs/react-router-dom'
 import { selectPostById } from './postSlice'
+import { Spinner } from '../../components/Spinner'
+import { useGetPostsQuery } from '../api/apiSlice'
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
 
-  const post = useSelector(state => selectPostById(state, postId))
+  const { data: post, isFetching, isSuccess } = useGetPostsQuery(postId)
 
-  if (!post) {
-    return (
-      <section>
-        <h2>Post not found!</h2>
-      </section>
-    )
-  }
-
-  return (
-    <section>
+  let content
+  if (isFetching) {
+    content = <Spinner text="Loading..." />
+  } else if (isSuccess) {
+    content = (
       <article className="post">
         <h2>{post.title}</h2>
+        <div>
+          <PostAuthor userId={post.user} />
+          <TimeAgo timestamp={post.date} />
+        </div>
         <p className="post-content">{post.content}</p>
+        <ReactionButtons post={post} />
         <Link to={`/editPost/${post.id}`} className="button">
           Edit Post
         </Link>
       </article>
-    </section>
-  )
+    )
+  }
+
+  return <section>{content}</section>
 }
